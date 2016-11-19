c22a5dbe958e76cc4eb910d200c2d5c79fd0adf9
Alan Russell <ajrussellaudio@gmail.com>
2016-11-19 18:08:13 +0000

    add seekbar to control pitch

--- app/src/main/java/com/ajrussellaudio/javascripttest/MainActivity.java
+++ app/src/main/java/com/ajrussellaudio/javascripttest/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +21,21 @@ public class MainActivity extends AppCompatActivity {
 
         webview.setWebViewClient(new WebViewClient());
         webview.loadUrl("file:///android_asset/index.html");
+
+        SeekBar slider = (SeekBar) findViewById(R.id.slider);
+        slider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            WebView webview = (WebView) findViewById(R.id.webview);
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                webview.loadUrl("javascript:sliderChange(" + progress + ")");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
     }
 }
