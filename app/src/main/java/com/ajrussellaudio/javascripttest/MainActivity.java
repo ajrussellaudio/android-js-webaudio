eca5c9e63dad3341a690669a11ed5c47a59f33b1
Alan Russell <ajrussellaudio@gmail.com>
2016-11-19 16:59:56 +0000

    initial commit

---
+++ app/src/main/java/com/ajrussellaudio/javascripttest/MainActivity.java
@@ -0,0 +1,24 @@
+package com.ajrussellaudio.javascripttest;
+
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webview = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webview.setWebViewClient(new WebViewClient());
+        webview.loadUrl("file:///android_asset/index.html");
+    }
+}
